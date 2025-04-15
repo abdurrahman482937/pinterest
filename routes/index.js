a5d87abf855489383703c5579408feb893c89d2c
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-15 19:39:09 +0600

    index..

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,16 @@ router.get('/', async function (req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/createuser', async function (req, res) {
+  const user = await userModel.create({
+    fullname: "Abdur Rahman",
+    username: "abdurrahman",
+    password: "pAss@123",
+    email: "abdurrahman@gmail.com ",
+    dp: "",
+    posts: [],
+  })
+  res.send(user)
+});
 
 module.exports = router;
